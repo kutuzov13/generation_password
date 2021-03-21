3514b57a26d599f60531f65b6e80e92869e66507
Yegor <kutuzov-1313@yandex.ru>
2021-03-21 18:26:02 +0300

    Refactoring

--- generation_password.py
+++ generation_password.py
@@ -9,7 +9,7 @@ chars = ''
 
 
 def generate_password(length, chars):
-    """Generation password """
+    """Generation password."""
     password = ''
     for _ in range(length): 
         password += random.choice(chars)
@@ -17,7 +17,7 @@ def generate_password(length, chars):
 
 
 def save_password(name):
-    """Saved password in new File"""
+    """Saved password in new File."""
     new_file = open('You_Password.txt', 'a')
     for _ in range(count):
         new_file.write(f'{name}: {generate_password(length, chars)}\n')
@@ -26,50 +26,50 @@ def save_password(name):
 
 
 try:
-    count = input('Какое колличество паролей сгенерировать:\n')
+    count = input('How many passwords to generate:\n')
     count = int(count)
 except ValueError:
-    count = input('Введите целое число!(Если программа не получит целове пароль не будет сгенерирован)\n')
+    count = input('Please enter an integer!\n')
     count = int(count)
 
 try:
-    length = input('Длинна пароля:\n')
+    length = input('Password length:\n')
     length = int(length)
 except ValueError:
-    length = input('Введите целое число!(Если программа не получит целове число будет сгенерирован пустой пароль)\n')
+    length = input('Please enter an integer!\n')
     length = int(length)
 
-in_numbers = input('Включать ли цифры 0123456789?\n')
+in_numbers = input('Should numbers be included 0123456789?\n')
 if in_numbers.lower() in ('Да', 'ДА', 'да', 'Yes', 'YES', 'yes'):
     chars += digits
 else:
     chars += '0'
 
-in_big_letters = input('Включить ли прописные буквы ABCDEFGHIJKLMNOPQRSTUVWXYZ?\n')
+in_big_letters = input('Whether to include uppercase letters ABCDEFGHIJKLMNOPQRSTUVWXYZ?\n')
 if in_big_letters.lower() in ('Да', 'ДА', 'да', 'Yes', 'YES', 'yes'):
     chars += uppercase_letters
 else:
     chars += '0'
 
-in_small_letter = input('Включать ли строчные буквы abcdefghijklmnopqrstuvwxyz?\n')
+in_small_letter = input('Should I include lowercase letters abcdefghijklmnopqrstuvwxyz?\n')
 if in_small_letter.lower() in ('Да', 'ДА', 'да', 'Yes', 'YES', 'yes'):
     chars += lowercase_letters
 else:
     chars += '0'
 
-symbols = input('Включать ли символы "<!#$%&*+-=?№/@_>" ?\n')
+symbols = input('Should symbols be included "<!#$%&*+-=?№/@_>" ?\n')
 if symbols.lower() in ('Да', 'ДА', 'да', 'Yes', 'YES', 'yes'):
     chars += punctuation
 else:
     chars += '0'
 
-name = input('Как назвать ваш пароль?\n')
+name = input('How to name your password?\n')
 
-save = input('Хотите сохранить пароль в файл?\n')
+save = input('Want to save your password to a file?\n')
 if save.lower() in ('Да', 'ДА', 'да', 'Yes', 'YES', 'yes'):
     save_password(name)
 else:
-    print(f'Ваш пароль: {generate_password(length, chars)}')
+    print(f'Your password: {generate_password(length, chars)}')
 
 # Генерация нужного количества паролей:
 for p in range(count):
