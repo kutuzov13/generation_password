8c8c0d5f50606d97f3981d970b632211691706e5
kutuzov_em <kutuzov-1313@yandex.ru>
2021-03-29 16:51:53 +0300

    update name constants

--- generation_password.py
+++ generation_password.py
@@ -1,13 +1,13 @@
 import random
 
 # Templates for generator
-digits = '0123456789'
-lowercase_letters = 'abcdefghijklmnopqrstuvwxyz'
-uppercase_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-punctuation = '!#$%&*+-=?@^_'
-chars = ''
+DIGITS = '0123456789'
+LOWERCASE_LETTERS = 'abcdefghijklmnopqrstuvwxyz'
+UPPERCASE_LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+PUNCTUATION = '!#$%&*+-=?@^_'
+CHARS = ''
 
-answer_options = ('Да', 'ДА', 'да', 'Yes', 'YES', 'yes', 'y', 'Y')
+ANSWER_OPTIONS = ('Да', 'ДА', 'да', 'Yes', 'YES', 'yes', 'y', 'Y')
 
 
 def generate_password(length_password, chars_password):
@@ -22,7 +22,7 @@ def save_password(name_password):
     """Saved password in new File."""
     new_file = open('You Password.txt', 'a')
     for _ in range(count):
-        new_file.write(f'{name_password}: {generate_password(length, chars)}\n')
+        new_file.write(f'{name_password}: {generate_password(length, CHARS)}\n')
     new_file.close()
     return new_file
 
@@ -42,37 +42,37 @@ except ValueError:
     length = int(length)
 
 in_numbers = input('Should numbers be included 0123456789?\n')
-if in_numbers.lower() in answer_options:
-    chars += digits
+if in_numbers.lower() in ANSWER_OPTIONS:
+    CHARS += DIGITS
 else:
-    chars += '0'
+    CHARS += '0'
 
 in_big_letters = input('Whether to include uppercase letters ABCDEFGHIJKLMNOPQRSTUVWXYZ?\n')
-if in_big_letters.lower() in answer_options:
-    chars += uppercase_letters
+if in_big_letters.lower() in ANSWER_OPTIONS:
+    CHARS += UPPERCASE_LETTERS
 else:
-    chars += '0'
+    CHARS += '0'
 
 in_small_letter = input('Should I include lowercase letters abcdefghijklmnopqrstuvwxyz?\n')
-if in_small_letter.lower() in answer_options:
-    chars += lowercase_letters
+if in_small_letter.lower() in ANSWER_OPTIONS:
+    CHARS += LOWERCASE_LETTERS
 else:
-    chars += '0'
+    CHARS += '0'
 
 symbols = input('Should symbols be included "<!#$%&*+-=?№/@_>" ?\n')
-if symbols.lower() in answer_options:
-    chars += punctuation
+if symbols.lower() in ANSWER_OPTIONS:
+    CHARS += PUNCTUATION
 else:
-    chars += '0'
+    CHARS += '0'
 
 user_name_password = input('How to user_name_password your password?\n')
 
 save = input('Want to save your password to a file?\n')
-if save.lower() in answer_options:
+if save.lower() in ANSWER_OPTIONS:
     save_password(user_name_password)
 else:
-    print(f'Your password: {generate_password(length, chars)}')
+    print(f'Your password: {generate_password(length, CHARS)}')
 
 # Generating the required number of passwords:
 for passwords in range(count):
-    generate_password(length, chars)
+    generate_password(length, CHARS)
